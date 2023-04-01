25b0b5b794c322db41a39d7a731933bed5915c0d
Techsations <akandedamilare.t@gmail.com>
2023-04-01 11:52:05 +0100

    I started declaring my Js Variables

---
+++ alarm.js
@@ -0,0 +1,7 @@
+let hour = document.getElementById('hours');
+let minutes = document.getElementById('mins');
+let seconds = document.getElementById('secs');
+let setHour = document.getElementById('alarmHour');
+let setMinutes = document.getElementById('alarmMinutes')
+let display = document.getElementById('set-Alarm')
+let audio = document.getElementById('playAudio')
