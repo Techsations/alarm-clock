0c0711d77c193310b5f7e20acf74ff42b42b9b40
Techsations <akandedamilare.t@gmail.com>
2023-04-01 11:53:47 +0100

    I added a function for time

--- alarm.js
+++ alarm.js
@@ -5,3 +5,22 @@ let setHour = document.getElementById('alarmHour');
 let setMinutes = document.getElementById('alarmMinutes')
 let display = document.getElementById('set-Alarm')
 let audio = document.getElementById('playAudio')
+
+function time() {
+    let date = new Date();
+    hour.innerHTML = date.getHours();
+    minutes.innerHTML = date.getMinutes();
+    seconds.innerHTML = date.getSeconds();
+    if (seconds.innerHTML < 10) {
+        seconds.innerHTML = "0" + seconds.innerHTML;
+    }
+
+    if (minutes.innerHTML < 10) {
+        minutes.innerHTML = "0" + minutes.innerHTML;
+    }
+
+    if (hour.innerHTML < 10) {
+        hour.innerHTML = "0" + hour.innerHTML;
+    }
+    }
+    setInterval(time, 1000)
